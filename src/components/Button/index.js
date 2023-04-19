41198c342e0a88671d11b5f69eea8e9e50b3c61b
huyenpt18102002 <105543582+huyenpt18102002@users.noreply.github.com>
2023-04-19 14:56:28 +0700

    update header

---
+++ src/components/Button/index.js
@@ -0,0 +1,67 @@
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    rounded = false,
+    disabled = false,
+    small = false,
+    large = false,
+    text = false,
+    className,
+    leftIcon,
+    rightIcon,
+    children,
+    onClick,
+    ...passProps
+}) {
+    let Comp = 'button';
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    //remove event when button is disabled
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline,
+        rounded,
+        disabled,
+        small,
+        large,
+        text,
+    });
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+import routesConfig from '~/config/routes';
 import Home from '../pages/Home';
 import Following from '../pages/Following';
 import Profile from '../pages/Profile';
@@ -6,11 +7,11 @@ import Search from '~/pages/Search';
 import { HeaderOnly } from '../components/Layout';
 //Public routes
 const publicRoutes = [
-    { path: '/', component: Home },
-    { path: '/following', component: Following },
-    { path: '/profile', component: Profile },
-    { path: '/upload', component: Upload, layout: HeaderOnly },
-    { path: '/search', component: Search, layout: null },
+    { path: routesConfig.home, component: Home },
+    { path: routesConfig.following, component: Following },
+    { path: routesConfig.profile, component: Profile },
+    { path: routesConfig.upload, component: Upload, layout: HeaderOnly },
+    { path: routesConfig.search, component: Search, layout: null },
 ];
 
 const privateRoutes = [];
